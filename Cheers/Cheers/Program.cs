26bc3a191c1e154925d520b08f87a7a8ffd2e038
BrendonPierson <brendonpierson@gmail.com>
2015-10-14 17:23:01 -0500

    Added birthday days countdown.

--- Cheers/Cheers/Program.cs
+++ Cheers/Cheers/Program.cs
@@ -12,8 +12,11 @@ namespace Cheers
         {
             System.Console.WriteLine("Hello there.  What's your name?");
             string name = System.Console.ReadLine();
-            name = name.ToLower();
             System.Console.WriteLine("Hi, " + name);
+            name = name.ToLower();
+
+            System.Console.WriteLine("What's your birthday? ex: 09/20");
+            string bdayString = System.Console.ReadLine();
 
             string vowels = "aeiouh";
 
@@ -21,8 +24,7 @@ namespace Cheers
             {
                 if (vowels.Contains(letter))
                 {
-                    System.Console.WriteLine("Give me an.. " + letter);
-              
+                    System.Console.WriteLine("Give me an.. " + letter);              
                 } 
                 else
                 {
@@ -30,6 +32,42 @@ namespace Cheers
                 }
             }
             System.Console.WriteLine(name.ToUpper() + " is.. GRAND!");
+      
+          
+            
+            int mon = Int32.Parse(bdayString.Substring(0, 2));
+            int day = Int32.Parse(bdayString.Substring(3, 2));
+
+            DateTime userBday = new DateTime(DateTime.Now.Year, mon, day);
+
+            DateTime now = DateTime.Now;
+
+            
+            int result = DateTime.Compare(userBday, now);
+
+            if(result == 0)
+            {
+                System.Console.WriteLine("Happy Birthday " + name + " !");
+            }
+            if(result > 0)
+            {
+                TimeSpan elapsed = userBday.Subtract(now);
+                double daysAgo = elapsed.TotalDays;
+                System.Console.WriteLine("It is " + daysAgo.ToString("0") + " days until your birthday.");
+            }
+            if(result < 0)
+            {
+                TimeSpan elapsed = now.AddYears(1).Subtract(userBday);
+                double daysAgo = elapsed.TotalDays;
+                System.Console.WriteLine("It is " + (daysAgo.ToString("0")) + " days until your birthday.");
+            }
+            
+            
+
+            
+
+            
+            
             System.Console.WriteLine("Press any key to exit.");
             System.Console.ReadKey();
         }
