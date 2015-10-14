66a7291dc1334287b4d401b0074f333e14c84046
BrendonPierson <brendonpierson@gmail.com>
2015-10-13 20:29:21 -0700

    Removed first letter capitalization as in the directions.

--- Cheers/Cheers/Program.cs
+++ Cheers/Cheers/Program.cs
@@ -16,35 +16,20 @@ namespace Cheers
             System.Console.WriteLine("Hi, " + name);
 
             string vowels = "aeiouh";
-            bool firstChar = true;
 
             foreach (char letter in name)
             {
                 if (vowels.Contains(letter))
                 {
-                    if (firstChar)
-                    {
-                        System.Console.WriteLine("Give me an " + letter.ToString().ToUpper() + "!");
-                        firstChar = false;
-                    } else
-                    {
-                        System.Console.WriteLine("Give me an " + letter + "!");
-                    }
+                    System.Console.WriteLine("Give me an.. " + letter);
+              
                 } 
                 else
                 {
-                    if (firstChar)
-                    {
-                        System.Console.WriteLine("Give me a " + letter.ToString().ToUpper() + "!");
-                        firstChar = false;
-                    }
-                    else
-                    {
-                        System.Console.WriteLine("Give me a " + letter + "!");
-                    }
+                    System.Console.WriteLine("Give me a.. " + letter);
                 }
             }
-            System.Console.WriteLine("Yay " + name.First().ToString().ToUpper() + name.Substring(1) + "!");
+            System.Console.WriteLine(name.ToUpper() + " is.. GRAND!");
             System.Console.WriteLine("Press any key to exit.");
             System.Console.ReadKey();
         }
